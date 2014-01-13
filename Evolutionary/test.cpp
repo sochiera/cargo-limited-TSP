cce71fbf70bd1c98f63e1acf072d9255cf38a10a
Jan Sochiera <jansochiera@gmail.com>
2014-01-13 21:40:37 +0100

    individualTestAdded

--- Evolutionary/test.cpp
+++ Evolutionary/test.cpp
@@ -17,6 +17,23 @@ TEST(IndividualTest, RandomIndividualHasVisitedDefinedNumberOfCities) {
     }
     ASSERT_EQ(sum, CITIES);
 }
+
+TEST(IndividualTest, RandomIndividualHasVisitedAllCities) {
+    Individual ind;
+    vector<int> cities;
+    vector<Individual::Course> courses = ind.getCourses();
+    for(vector<Individual::Course>::iterator it = courses.begin(); it != courses.end(); it++){
+        for(vector<int>::iterator cit = it->begin(); cit != it->end(); cit++){
+            cities.push_back(*cit);
+        }
+    }
+    sort(cities.begin(), cities.end());
+    
+    ASSERT_EQ(cities.size(), CITIES);
+    for(int i = 0; i < CITIES; i++){
+        ASSERT_EQ(cities[i], i);
+    }
+}
  
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
